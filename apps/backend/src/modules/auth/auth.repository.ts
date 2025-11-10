578c1b7f8a1934ccbfdf0b4dd7313e0f657b4b37
Nailyn <ntabeldinov2002@gmail.com>
2025-11-10 15:06:45 +0000

    chore: migrate from pnpm to npm (remove pnpm files, add npm lock)

--- apps/backend/src/modules/auth/auth.repository.ts
+++ apps/backend/src/modules/auth/auth.repository.ts
@@ -1,6 +1,5 @@
 import { LoginInput, RegisterInput } from "./auth.schema.js";
 import prisma from "../../prisma/prisma.service.js";
-
 export const authRepository = {
   createUser: async (data: RegisterInput) => {
     return prisma.user.create({ data });
