b618221fc97c28d52711d1ca3e0e41a04e2836a7
Nailyn <ntabeldinov2002@gmail.com>
2025-12-11 01:40:26 +0000

    feat(idempotency) added requestHash

--- apps/backend/src/middleware/withIdempotency.ts
+++ apps/backend/src/middleware/withIdempotency.ts
@@ -1,5 +1,6 @@
 import { Response, Request, NextFunction } from "express";
 import prisma from "../prisma/prisma.service.js";
+import crypto from "crypto";
 
 export const idempotencyMiddleware = async (
   req: Request,
@@ -16,28 +17,43 @@ export const idempotencyMiddleware = async (
 
     const endpoint = req.originalUrl;
 
+    const requestBodyString = JSON.stringify(req.body ?? {});
+    const requestHash = crypto
+      .createHash("sha256")
+      .update(requestBodyString)
+      .digest("hex");
+
     const record = await prisma.idempotencyRecord.findUnique({
       where: { key },
     });
-
     if (record) {
       if (record.locked) {
         return res
           .status(409)
           .json({ message: "Request is already in progress" });
       }
+
+      if (record.requestHash !== requestHash) {
+        return res.status(409).json({
+          message: "Idempotency-Key already used for a different request body",
+        });
+      }
+
       if (record.responseBody && record.responseStatus) {
         if (typeof record.responseBody === "string") {
           const responseData = JSON.parse(record.responseBody);
           return res.status(record.responseStatus).json(responseData);
         }
       }
-    } else {
+    }
+
+    if (!record) {
       await prisma.idempotencyRecord.create({
         data: {
           key,
           endpoint,
           locked: true,
+          requestHash,
         },
       });
     }
